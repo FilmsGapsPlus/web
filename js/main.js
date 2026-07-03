48d457218c471f3e2ab666c1d550e680276ad0dc
FilmsGapsPlus <filmsgapsplusdevelopers@gmail.com>
2026-07-03 15:27:06 -0400

    Update comment for API base URL

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const countriesGridModal = document.getElementById("countries-grid-modal")
     const countriesSearch = document.getElementById("countries-search")
 
-    // URL base inicial
+    // URL base
     let apiBaseUrl = "https://anusdbs.onrender.com";
 
     // Variables para las URLs de la API (se inicializarán después de obtener la URL base)
